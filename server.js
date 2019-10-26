9251b49dcc2d5d396d879c07c55e0fae5440dea0
Doowankim <rlaendhks11@gmail.com>
2019-10-26 14:31:33 +0900

    add modules

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log("MongoDB Connected..."))
+        .catch(err => console.log(err));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+const port = process.env.PORT || 4000;
+app.listen(port, console.log(`Server running on port ${port}`));
