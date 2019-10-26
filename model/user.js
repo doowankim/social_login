638a47371b33dd1bba0d3e09cb933ffef3710877
Doowankim <rlaendhks11@gmail.com>
2019-10-26 15:10:39 +0900

    add password bcryptjs

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema({
 
     username: {
@@ -20,4 +21,22 @@ const userSchema = mongoose.Schema({
         default: Date.now
     }
 });
+
+userSchema.pre('save', async function(next){
+    try{
+        console.log('entered'); //저장을 할때 내보내는 메시지
+        const salt = await bcrypt.genSalt(10);
+
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        this.password = passwordHash; //passwordHash가 this.password에 들어가고 위의 password에 암호화되어 들어간다
+
+        console.log('Exited');
+        next();
+    }
+    catch(error){
+        next(error); //error를 내보낸다
+    }
+});
+
+
 module.exports = mongoose.model('user', userSchema);
